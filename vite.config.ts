6c49819c18e252ba1b614ee6fa9aa8c70d12d4d4
MAira <internetacademyoficial@gmail.com>
2024-12-04 12:40:41 -0300

    v0

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Certifique-se de que a pasta de build está correta
-  },
-  server: {
-    host: true,  // Permite acesso externo
-    port: 3000
-  }
 })
