dd69f4e4115c74db6b1204d4d6d9b93aa9d134ae
IA-Academy <139231248+IA-Academy@users.noreply.github.com>
2024-12-05 18:56:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir arquivos estáticos do diretório dist
 app.use(express.static('dist'));
 
-// Todas as rotas retornam o index.html
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
