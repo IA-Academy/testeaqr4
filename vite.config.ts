547493b532f084c27a9df11df7db006c5f46324e
IA-Academy <139231248+IA-Academy@users.noreply.github.com>
2024-12-05 08:40:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-})
+  export default defineConfig({
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0', // Permitir conexões externas
+      port: parseInt(process.env.PORT) || 3000, // Usar a porta especificada pela variável de ambiente PORT
+    },
+    preview: {
+      port: parseInt(process.env.PORT) || 3000, // Usar a porta especificada pela variável de ambiente PORT
+    }
+  })
