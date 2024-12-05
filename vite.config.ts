c29929967e8c2cc3fb2b790347ff39e0a0224954
IA-Academy <139231248+IA-Academy@users.noreply.github.com>
2024-12-05 18:55:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: true
   }
 })
