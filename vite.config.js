e980955104770d1d50a81a81654cb0dc88409eee
IA-Academy <139231248+IA-Academy@users.noreply.github.com>
2024-12-07 12:02:24 -0300

    Update and rename server.js to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+{
+  "name": "archetype-test",
+  "private": true,
+  "version": "0.0.0",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "@phosphor-icons/react": "^2.0.10",
+    "framer-motion": "^10.16.4",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "recharts": "^2.8.0",
+    "styled-components": "^6.0.7"
+  },
+  "devDependencies": {
+    "@vitejs/plugin-react": "^4.0.3",
+    "vite": "^4.4.5"
+  }
+}
