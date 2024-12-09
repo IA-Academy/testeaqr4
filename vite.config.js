82e60b64862aba60cec0af77c22ac823804811c2
MAira <internetacademyoficial@gmail.com>
2024-12-09 09:37:07 -0300

    v0

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
-{
-  "name": "archetype-test",
-  "private": true,
-  "version": "0.0.0",
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "preview": "vite preview"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
   },
-  "dependencies": {
-    "@phosphor-icons/react": "^2.0.10",
-    "framer-motion": "^10.16.4",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "recharts": "^2.8.0",
-    "styled-components": "^6.0.7"
-  },
-  "devDependencies": {
-    "@vitejs/plugin-react": "^4.0.3",
-    "vite": "^4.4.5"
-  }
-}
+});
